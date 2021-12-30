d36121094388ff7a9bd8a9621a70e16dfc8a514f
PraiseGod Adesanmi <daryor1000@gmail.com>
2021-12-30 23:55:31 +0100

    adding more pages

--- plep_app/urls.py
+++ plep_app/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
+from .apps import PlepAppConfig
+
+app_name = PlepAppConfig.name
 
 urlpatterns = [
      path('', views.index, name="home"),
-     path('csc320/', views.csc320, name='"csc320"'),
+     path('csc320/', views.csc320, name="csc320"),
      path('register/', views.register, name="register"),
      path('uid_verification/', views.uid_verification, name="uid_verification")
 ]
--- plep_project/urls.py
+++ plep_project/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('plep_app.urls'))
+    path('', include('plep_app.urls', namespace="plep_app"))
 ]
